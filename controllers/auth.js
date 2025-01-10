e2563a99eba97b0f6213071ad3c757145b4ea87b
Alan Vega <alan.vega@outlook.es>
2025-01-09 21:21:10 -0600

    password encrypt and add JWT feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,37 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async (req, res = response) => {
+    const { email, password } = req.body;
+
     try {
-        const user = new User(req.body);
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'The user already exists'
+            })
+        };
+
+        user = new User(req.body);
+
+        // Encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
 
         await user.save();
 
+        // Generate JWT
+        const token = await generateJWT(user.id, user.name);
+
         res.status(200).json({
             ok: true,
-            msg: 'login'
+            uid: user.id,
+            name: user.name,
+            token
         });
     } catch (error) {
         console.log(error);
@@ -21,13 +42,44 @@ const createUser = async (req, res = response) => {
     }
 }
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: 'login'
-    });
+    try {
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Invalid user or password.'
+            });
+        }
+
+        const passwordValid = bcrypt.compareSync(password, user.password);
+        if (!passwordValid) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Invalid user or password'
+            })
+        }
+
+        // Generate JWT
+        const token = await generateJWT(user.id, user.name);
+
+        res.status(200).json({
+            ok: true,
+            name: user.name,
+            uid: user.id,
+            token
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the administrator'
+        });
+    }
 }
 
 const tokenValid = (req, res = response) => {
